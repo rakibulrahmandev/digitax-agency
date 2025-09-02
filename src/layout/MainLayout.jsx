aad3e1a2f5a456467e14d44a3bc90d28f7e6c230
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-09-02 17:01:35 +0600

    imported Header.jsx component and routes in MainLayout.jsx file

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,7 +1,13 @@
+import { Route, Routes } from "react-router";
+import Header from "../components/Header/Header";
+
 export default function MainLayout() {
     return (
         <>
-
+            <Header/>
+            <Routes>
+                <Route path="/" element={''}></Route>
+            </Routes>
         </>
     );
 };
