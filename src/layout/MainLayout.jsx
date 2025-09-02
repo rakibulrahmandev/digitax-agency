7b5bcabe1aa5639b3ce10bffb959b5516e6198fa
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-09-02 19:27:47 +0600

    imported Home.jsx component in the MainLayout.jsx component and set as a home route element

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router";
 import Header from "../components/Header/Header";
+import Home from "../components/Home/Home/Home";
 
 export default function MainLayout() {
     return (
         <>
             <Header/>
             <Routes>
-                <Route path="/" element={''}></Route>
+                <Route path="/" element={<Home/>}></Route>
             </Routes>
         </>
     );
