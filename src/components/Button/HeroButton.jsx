3afaaa06481aef364d5f8e99c57c0bd00b335c67
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-09-02 21:27:30 +0600

    HeroButton design completed as look as a design

--- src/components/Button/HeroButton.jsx
+++ src/components/Button/HeroButton.jsx
@@ -1,7 +1,8 @@
-export default function HeroButton({type, text}) {
+export default function HeroButton({text}) {
     return (
         <>
-
+            <button className="py-3.5 px-5 rounded-full bg-absolute-white border border-absolute-white outline-none text-grey-shade-10 text-sm font-normal leading-[150%] cursor-pointer desktop:text-lg desktop:py-[18px] desktop:px-6" >{text}</button>
+            <button className="py-3.5 px-5 rounded-full bg-grey-shade-10 border border-grey-shade-15 outline-none text-absolute-white text-sm font-normal leading-[150%] cursor-pointer desktop:text-lg desktop:py-[18px] desktop:px-6" >{text}</button>
         </>
     );
 };
