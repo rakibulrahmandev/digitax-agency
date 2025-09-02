88f0ecab749d4631621e26307393d72b33a95a4a
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-09-02 17:09:48 +0600

    create NavLink.js file in the constants folder

---
+++ src/constants/NavLink.js
@@ -0,0 +1,37 @@
+export const navLinks = [
+    {
+        id: 1,
+        label: 'Home',
+        pathname: '/'
+    },
+    {
+        id: 2,
+        label: 'Services',
+        pathname: '/services'
+    },
+    {
+        id: 3,
+        label: 'Projects',
+        pathname: '/projects'
+    },
+    {
+        id: 4,
+        label: 'About Us',
+        pathname: '/about_us'
+    },
+    {
+        id: 5,
+        label: 'Contact Us',
+        pathname: '/contact_us'
+    },
+    {
+        id: 6,
+        label: 'Careers',
+        pathname: '/careers',
+    },
+    {
+        id: 7,
+        label: 'Blogs',
+        pathname: '/blogs'
+    }
+];
