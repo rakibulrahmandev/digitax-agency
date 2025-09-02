73f6694e0fd01805170c569b2a3cdee983705bc3
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-09-02 16:50:43 +0600

    added routes and route in App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Route, Routes } from "react-router";
 import Header from "./components/Header/Header";
 
 export default function App() {
     return (
         <>
             <Header/>
+            <Routes>
+                <Route path="/" element={''} />
+            </Routes>
         </>
     );
 };
