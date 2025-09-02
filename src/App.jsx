86ce3c7437202ea9196081bb5f4d819190ed4811
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-09-02 17:03:25 +0600

    added background color App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import MainLayout from "./layout/MainLayout";
 export default function App() {
     return (
         <>
-            <MainLayout/>
+            <div className="w-full min-h-screen bg-grey-shade-06">
+                <MainLayout/>
+            </div>
         </>
     );
 };
