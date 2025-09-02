a07235c0940a897268fb3a51dfdd8ebd657bb705
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-09-02 19:29:45 +0600

    imported HeroSection.jsx component in the Home.jsx file

--- src/components/Home/Home/Home.jsx
+++ src/components/Home/Home/Home.jsx
@@ -1,7 +1,9 @@
+import HeroSection from "../HeroSection/HeroSection";
+
 export default function Home() {
     return (
         <>
-            
+            <HeroSection/>
         </>
     );
 };
