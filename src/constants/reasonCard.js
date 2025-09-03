9e7815b49a571dcca9f403130e089c52a368b853
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-09-03 13:20:36 +0600

    imported and added ReasonButton in reasonCard.js file

--- src/constants/reasonCard.js
+++ src/constants/reasonCard.js
@@ -4,44 +4,51 @@ import icon_3 from '../assets/icons/reason_section_icons/icon_3.png'
 import icon_4 from '../assets/icons/reason_section_icons/icon_4.png'
 import icon_5 from '../assets/icons/reason_section_icons/icon_5.png'
 import icon_6 from '../assets/icons/reason_section_icons/icon_6.png'
+import ReasonButton from '../components/Button/ReasonButton'
 
 const reason_details = [
     {
         id: 1,
         icon: icon_1,
         text: 'Expertise That Drives Results',
-        para: `Our team of seasoned professionals brings years of experience and expertise to the table.`
+        para: `Our team of seasoned professionals brings years of experience and expertise to the table.`,
+        button: <ReasonButton/>
     },
     {
         id: 2,
         icon: icon_2,
         text: 'Tailored Business Solutions',
-        para: `We understand that every business is unique. That's why our solutions are customized.`
+        para: `We understand that every business is unique. That's why our solutions are customized.`,
+        button: <ReasonButton/>
     },
     {
         id: 3,
         icon: icon_3,
         text: 'Cutting-Edge Web Design',
-        para: `Leave a lasting impression on your audience with our top-notch web design services.`
+        para: `Leave a lasting impression on your audience with our top-notch web design services.`,
+        button: <ReasonButton/>
     },
     {
         id: 4,
         icon: icon_4,
         text: 'Mobile-First Approach',
-        para: `In today's mobile-centric world, we prioritize mobile-first design to ensure your website.`
+        para: `In today's mobile-centric world, we prioritize mobile-first design to ensure your website.`,
+        button: <ReasonButton/>
     },
     {
         id: 5,
         icon: icon_5,
         text: 'Marketing Strategies',
-        para: `Our data-driven marketing strategies allow us to target the right audience with precision.`
+        para: `Our data-driven marketing strategies allow us to target the right audience with precision.`,
+        button: <ReasonButton/>
     },
     {
         id: 6,
         icon: icon_6,
         text: 'Search Engine Optimization',
-        para: `(SEO) Mastery Boost your online visibility with our expert SEO techniques.`
-    },
+        para: `(SEO) Mastery Boost your online visibility with our expert SEO techniques.`,
+        button: <ReasonButton/>
+    }
 ];
 
 export default reason_details;
