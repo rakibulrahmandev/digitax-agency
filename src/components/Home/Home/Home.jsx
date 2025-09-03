fc79d63437150d5f9452cda9d6ebf605a156320f
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-09-03 12:49:29 +0600

    imported ReasonSection.jsx component in the Home.jsx file

--- src/components/Home/Home/Home.jsx
+++ src/components/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import HeroSection from "../HeroSection/HeroSection";
+import ReasonSection from "../ReasonSection/ReasonSection";
 
 export default function Home() {
     return (
         <>
             <div className="w-full flex items-center justify-center">
                 <HeroSection/>
+                <ReasonSection/>
             </div>
         </>
     );
