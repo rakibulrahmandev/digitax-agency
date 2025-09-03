05ad991b2ebc09758cc7ce4920ed6a60edb377fd
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-09-03 13:16:39 +0600

    here reasonCard.js file create card details array with icons from assets file

---
+++ src/constants/reasonCard.js
@@ -0,0 +1,47 @@
+import icon_1 from '../assets/icons/reason_section_icons/icon_1.png'
+import icon_2 from '../assets/icons/reason_section_icons/icon_2.png'
+import icon_3 from '../assets/icons/reason_section_icons/icon_3.png'
+import icon_4 from '../assets/icons/reason_section_icons/icon_4.png'
+import icon_5 from '../assets/icons/reason_section_icons/icon_5.png'
+import icon_6 from '../assets/icons/reason_section_icons/icon_6.png'
+
+const reason_details = [
+    {
+        id: 1,
+        icon: icon_1,
+        text: 'Expertise That Drives Results',
+        para: `Our team of seasoned professionals brings years of experience and expertise to the table.`
+    },
+    {
+        id: 2,
+        icon: icon_2,
+        text: 'Tailored Business Solutions',
+        para: `We understand that every business is unique. That's why our solutions are customized.`
+    },
+    {
+        id: 3,
+        icon: icon_3,
+        text: 'Cutting-Edge Web Design',
+        para: `Leave a lasting impression on your audience with our top-notch web design services.`
+    },
+    {
+        id: 4,
+        icon: icon_4,
+        text: 'Mobile-First Approach',
+        para: `In today's mobile-centric world, we prioritize mobile-first design to ensure your website.`
+    },
+    {
+        id: 5,
+        icon: icon_5,
+        text: 'Marketing Strategies',
+        para: `Our data-driven marketing strategies allow us to target the right audience with precision.`
+    },
+    {
+        id: 6,
+        icon: icon_6,
+        text: 'Search Engine Optimization',
+        para: `(SEO) Mastery Boost your online visibility with our expert SEO techniques.`
+    },
+];
+
+export default reason_details;
